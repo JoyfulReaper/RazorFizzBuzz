dda5a353feeb0c0702530ac6b8f3bc1811b53630
Kyle Givler <xvegan88x@gmail.com>
2021-08-05 15:52:30 -0400

    Initial Solution

--- RazorFizzBuzz/Pages/FizzBuzz.cshtml.cs
+++ RazorFizzBuzz/Pages/FizzBuzz.cshtml.cs
@@ -4,13 +4,55 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorFizzBuzz.Models;
 
 namespace RazorFizzBuzz.Pages
 {
     public class FizzBuzzModel : PageModel
     {
+        [BindProperty]
+        public FizzBuzz FizzBuzz { get; set; }
+
         public void OnGet()
         {
+            FizzBuzz = new FizzBuzz
+            {
+                FizzValue = 3,
+                BuzzValue = 5
+            };
+        }
+
+        public void OnPost()
+        {
+            List<string> fbItems = new();
+
+            bool isFizz;
+            bool isBuzz;
+
+            for (int i = 1; i <= 100; i++)
+            {
+                isFizz = i % FizzBuzz.FizzValue == 0;
+                isBuzz = i % FizzBuzz.BuzzValue == 0;
+
+                if (isBuzz && isFizz)
+                {
+                    fbItems.Add("FizzBuzz");
+                }
+                else if (isFizz)
+                {
+                    fbItems.Add("Fizz");
+                }
+                else if (isBuzz)
+                {
+                    fbItems.Add("Buzz");
+                }
+                else
+                {
+                    fbItems.Add(i.ToString());
+                }
+            }
+
+            FizzBuzz.Result = fbItems;
         }
     }
 }
